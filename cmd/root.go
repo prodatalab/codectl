badd3345a90a9376f5b82aab3dce60001d789fd1
Peter Alexander (ProDataLab) <floss@prodatalab.com>
2018-01-30 08:57:52 -0500

    added create command scaffolding

--- cmd/root.go
+++ cmd/root.go
@@ -28,16 +28,11 @@ var cfgFile string
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "codectl",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
+	Short: "codectl controls the CodeDepot cluster manager.",
+	Long: `codectl controls the CodeDepot cluster manager. 
 
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
-	//	Run: func(cmd *cobra.Command, args []string) { },
+  Find more information at https://github.com/codedepot/codectl.
+	`,
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
